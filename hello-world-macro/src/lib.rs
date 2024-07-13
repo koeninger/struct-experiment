0a35b97539127c5d018442c2e1c3802ff9146e70
cody koeninger <cody@koeninger.org>
2024-07-12 20:32:31 -0500

    trim down example

--- hello-world-macro/src/lib.rs
+++ hello-world-macro/src/lib.rs
@@ -1,65 +1,8 @@
 use quote::quote;
-use proc_macro::{TokenStream, TokenTree};
+use proc_macro::{TokenStream};
 use proc_macro2::{TokenStream as TS, TokenTree as TT};
 use proc_macro2::{Ident, Span};
-use syn::{parse_macro_input, DeriveInput};
 use std::collections::HashSet;
-
-#[proc_macro_derive(Hello)]
-pub fn hello(input: TokenStream) -> TokenStream {
-    println!("in hello: {input:?}");
-    let ast = parse_macro_input!(input as DeriveInput);
-    let name = ast.ident;
-    let hw = quote! {
-        impl #name {
-            fn hello_world(&self) {
-                println!("Hello macro world");
-            }
-        }
-    };
-    hw.into()
-}
-
-#[proc_macro_derive(HelloAlt)]
-pub fn hello_alt(input: TokenStream) -> TokenStream {
-    fn ident_name(tt: TokenTree) -> String {
-        match tt {
-            TokenTree::Ident(i) => i.to_string(),
-            _ => panic!("no ident")
-        }
-    }
-    let name = ident_name(input.into_iter().nth(1).unwrap());
-
-    format!("
-impl {name} {{
-  fn hello_world_alt(&self) {{
-    println!(\"Hello manual macro world\");
-  }}
-}}").parse().unwrap()
-}
-
-#[proc_macro]
-pub fn biscuit(input: TokenStream) -> TokenStream {
-    let input: TS = input.into();
-    println!("biscuit inputs: {input:?}");
-    let idents: Vec<_> = input.into_iter().filter_map(|x| match x {
-        TT::Ident(i) => Some(quote!{
-            #i: u32
-        }),
-        _ => None
-    }).collect();
-    println!("biscuit idents: {idents:?}");
-    let foo = Ident::new("foo", Span::call_site());
-    let footyp = Ident::new("i64", Span::call_site());
-    let out = quote! {
-        #[derive(Debug, Default)]
-        struct Biscuit {
-            #foo : #footyp,
-            #( #idents ,)*
-        }
-    };
-    out.into()
-}
 
 const OK_FIELDS: &[(&'static str, &'static str)] = &[
     ("checksum", "u32"),
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,17 @@
 #[macro_use]
 extern crate hello_world_macro;
 
-#[derive(Hello, HelloAlt)]
-struct Example;
-
-biscuit!(jam, jelly);
-
 ok_struct!(Buy, checksum, global_seq_num, tstamp);
 ok_struct!(Ping, checksum);
 ok_struct!(Wtf, tstamp, checksum);
 
 fn main() {
-    let e = Example {};
-    e.hello_world();
-
-    let b: Biscuit = Default::default();
+    let b = Buy { global_seq_num: 23, tstamp: 42, ..Default::default() };
     println!("{b:?}");
 
-    let b = Buy { global_seq_num: 23, ..Default::default() };
-    println!("{b:?}");
+    let p: Ping = Default::default();
+    println!("{p:?}");
+
+    let w = Wtf { tstamp: 42, ..Default::default() };
+    println!("{w:?}");
 }
