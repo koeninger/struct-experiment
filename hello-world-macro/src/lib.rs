409c8d96330d59a26276313258816af7f8cdd5c3
cody koeninger <cody@koeninger.org>
2024-07-12 21:11:15 -0500

    error on unexpected fields

--- hello-world-macro/src/lib.rs
+++ hello-world-macro/src/lib.rs
@@ -15,10 +15,11 @@ pub fn ok_struct(input: TokenStream) -> TokenStream {
     let input: TS = input.into();
     let mut idents = input.into_iter().filter(|x| matches!(x, TT::Ident(_)));
     let name = idents.next().expect("ok_struct expects at least one ident for the struct name");
-    let wanted: HashSet<_> = idents.map(|x| x.to_string()).collect();
+    let mut wanted: HashSet<_> = idents.map(|x| x.to_string()).collect();
     let fields: Vec<_> = OK_FIELDS.iter().enumerate().map(|(i, (f, t))| {
         let f = *f;
         let f = if wanted.contains(f) {
+            wanted.remove(f);
             Ident::new(f, Span::call_site())
         } else {
             Ident::new(&format!("padding_{i}"), Span::call_site())
@@ -26,6 +27,9 @@ pub fn ok_struct(input: TokenStream) -> TokenStream {
         let t = Ident::new(t, Span::call_site());
         quote! { #f : #t }
     }).collect();
+    if !wanted.is_empty() {
+        panic!("ok_struct fields for {name} are not ok: {wanted:?}");
+    }
     let out = quote! {
         #[repr(C)]
         #[derive(Clone, Copy, Debug, Default)]
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 #[macro_use]
 extern crate hello_world_macro;
 
-type Checksum = u32;
+#[derive(Clone, Copy, Debug, Default)]
+struct Checksum(u32);
 
 ok_struct!(Buy, checksum, global_seq_num, tstamp);
 ok_struct!(Ping, checksum);
+// will fail
+//ok_struct!(Wtf, tstamp, checksum, some_junk_field);
 ok_struct!(Wtf, tstamp, checksum);
 
 fn main() {
